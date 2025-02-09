d5ea03702922b96c9f7c8b29a1bf2a4c43febf63
Masterchef365 <duncan.freeman1@gmail.com>
2025-02-08 22:08:49 -0800

    Cleanup

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -53,6 +53,7 @@ impl eframe::App for TemplateApp {
     */
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.request_repaint();
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
@@ -139,26 +140,20 @@ fn interact_with_component(
     let begin = cellpos_to_egui(comp.begin);
     let end = cellpos_to_egui(comp.end);
     let body_rect = Rect::from_points(&[begin, end]);
-    let mut body_rect = body_rect.expand(10.0);
+    let body_hitbox = body_rect.expand(10.0);
 
     let handle_hitbox_size = 50.0;
-    let begin_handle = Rect::from_center_size(begin, Vec2::splat(handle_hitbox_size));
-    let end_handle = Rect::from_center_size(end, Vec2::splat(handle_hitbox_size));
+    let begin_hitbox = Rect::from_center_size(begin, Vec2::splat(handle_hitbox_size));
+    let end_hitbox = Rect::from_center_size(end, Vec2::splat(handle_hitbox_size));
 
-    let body_resp = ui.allocate_rect(body_rect, Sense::click_and_drag());
-    let end_resp = ui.allocate_rect(end_handle, Sense::click_and_drag());
-    let begin_resp = ui.allocate_rect(begin_handle, Sense::click_and_drag());
+    let body_resp = ui.allocate_rect(body_hitbox, Sense::FOCUSABLE | Sense::click_and_drag());
 
-    let focus = |resp: egui::Response| {
-        if resp.clicked() {
-            resp.request_focus();
-        }
-        if resp.has_focus() {
-            Color32::RED
-        } else {
-            Color32::WHITE
-        }
-    };
+    if body_resp.clicked() {
+        body_resp.request_focus();
+    }
+
+    let end_resp = ui.allocate_rect(end_hitbox, Sense::click_and_drag());
+    let begin_resp = ui.allocate_rect(begin_hitbox, Sense::click_and_drag());
 
     let interact_pos = body_resp.interact_pointer_pos().or(begin_resp.interact_pointer_pos()).or(end_resp.interact_pointer_pos());
 
@@ -184,35 +179,33 @@ fn interact_with_component(
     }
 
     if body_resp.drag_stopped() || begin_resp.drag_stopped() || end_resp.drag_stopped() {
-        comp.begin = egui_to_cellpos(begin + dbg!(begin_offset));
-        comp.end = egui_to_cellpos(end + dbg!(end_offset));
+        comp.begin = egui_to_cellpos(begin + begin_offset);
+        comp.end = egui_to_cellpos(end + end_offset);
     }
 
     if body_resp.drag_stopped() || begin_resp.drag_stopped() || end_resp.drag_stopped() {
         ui.memory_mut(|mem| mem.data.remove::<Pos2>(id));
     }
 
-    let body_color = focus(body_resp.clone());
+    let has_focus = body_resp.has_focus();
     ui.painter().rect_stroke(
-        body_rect.translate((begin_offset + end_offset) / 2.0),
+        body_hitbox.translate((begin_offset + end_offset) / 2.0),
         0.0,
-        Stroke::new(1., body_color),
+        Stroke::new(1., if has_focus { Color32::RED } else { Color32::WHITE }),
         egui::StrokeKind::Inside,
     );
 
-    let begin_color = focus(begin_resp);
     ui.painter().rect_stroke(
-        begin_handle.translate(begin_offset),
+        begin_hitbox.translate(begin_offset),
         0.0,
-        Stroke::new(1., begin_color),
+        Stroke::new(1., Color32::WHITE),
         egui::StrokeKind::Inside,
     );
 
-    let end_color = focus(end_resp);
     ui.painter().rect_stroke(
-        end_handle.translate(end_offset),
+        end_hitbox.translate(end_offset),
         0.0,
-        Stroke::new(1., end_color),
+        Stroke::new(1., Color32::WHITE),
         egui::StrokeKind::Inside,
     );
 
@@ -220,115 +213,4 @@ fn interact_with_component(
 
 
     body_resp
-    /*
-
-    let drag_begin_pos = ui.memory_mut(|mem| *mem.data.get_temp_mut_or_default::<Vec2>(id));
-
-    let begin = cellpos_to_egui(comp.begin);
-    let end = cellpos_to_egui(comp.end);
-
-    let mut body_rect = Rect::from_points(&[drag_begin_pos, (begin - end) + drag_begin_pos]);
-    // Expand dong.
-    let mut body_rect = body_rect.expand(10.0);
-
-    let body_resp = ui.allocate_rect(body_rect, Sense::click_and_drag());
-
-    if body_resp.clicked() {
-        body_resp.request_focus();
-    }
-
-    if body_resp.has_focus() {
-        if body_resp.drag_started() {
-            if let Some(interact) = body_resp.interact_pointer_pos() {
-                ui.memory_mut(|mem| *mem.data.get_temp_mut_or_default::<Vec2>(id) = interact - begin);
-            }
-        }
-
-        if body_resp.dragged() {
-            ui.memory_mut(|mem| *mem.data.get_temp_mut_or_default::<Vec2>(id) += delta);
-        }
-    } else {
-        let offset = ui.memory_mut(|mem| {
-            let val = mem.data.get_temp_mut_or_default::<Vec2>(id);
-            let tmp = *val;
-            *val = Vec2::ZERO;
-            tmp
-        });
-
-        egui_to_cellvec(pos)
-    }
-
-    body_resp
-    */
-
-    /*
-    //ui.memory_mut(|mem| mem.data.remove::<Vec2>("component body".into()));
-    if body_resp.has_focus() {
-        //let begin = drag_handle(ui, &mut comp.begin);
-        //let end = drag_handle(ui, &mut comp.end);
-        if dbg!(body_resp.dragged()) {
-            let delta = body_resp.drag_delta();
-            dbg!(delta);
-
-            let cellpos_delta = ui.memory_mut(|mem| {
-                let offset = mem.data.get_temp_mut_or_default::<Vec2>("component body".into());
-                *offset += delta;
-                let floor = egui_to_cellvec(*offset);
-                let fract = *offset - cellpos_to_egui_vec(floor);
-                *offset = dbg!(fract);
-                floor
-            });
-
-            dbg!(cellpos_delta);
-
-            comp.begin.0 += cellpos_delta.0;
-            comp.begin.1 += cellpos_delta.1;
-            comp.end.0 += cellpos_delta.0;
-            comp.end.1 += cellpos_delta.1;
-        }
-    }
-
-    let body_color = if body_resp.has_focus() {
-        Color32::BLUE
-    } else {
-        Color32::LIGHT_GRAY
-    };
-
-    ui.painter().line_segment(
-        [cellpos_to_egui(comp.begin) + offset, cellpos_to_egui(comp.end) + offset],
-        Stroke::new(1., body_color),
-    );
-
-    body_resp
-    */
-}
-
-fn drag_handle(ui: &mut Ui, pos: &mut CellPos) -> egui::Response {
-    let handle_hitbox_size = 10.0;
-
-    let egui_pos = cellpos_to_egui(*pos);
-
-    let rect = Rect::from_center_size(egui_pos, Vec2::splat(handle_hitbox_size));
-
-    let resp = ui.allocate_rect(rect, Sense::FOCUSABLE | Sense::DRAG | Sense::CLICK);
-
-    let handle_radius = 10.0;
-    let handle_color = if resp.has_focus() {
-        Color32::RED
-    } else {
-        Color32::WHITE
-    };
-
-    if resp.hovered() || resp.has_focus() {
-        ui.painter()
-            .circle_filled(egui_pos, handle_radius, handle_color);
-    }
-
-    if resp.dragged() || resp.clicked() {
-        resp.request_focus();
-    }
-
-    if resp.has_focus() {}
-
-    resp
 }
